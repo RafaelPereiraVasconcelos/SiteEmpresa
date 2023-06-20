75f06a3fe1edbdbc0b0093460719f5fee154e7d6
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-06-20 00:57:01 -0300

    Footer e Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProdutoComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
